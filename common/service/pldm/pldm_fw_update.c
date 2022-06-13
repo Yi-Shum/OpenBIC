c0df628af85b752ad7ff42537e4ef83a40ab300b
MouchenHung <Mouchen.Hung@quantatw.com>
2022-06-13 20:25:49 +0800

    add request update command

--- common/service/pldm/pldm_fw_update.c
+++ common/service/pldm/pldm_fw_update.c
@@ -9,7 +9,81 @@
 
 LOG_MODULE_DECLARE(pldm, LOG_LEVEL_DBG);
 
-static pldm_cmd_handler pldm_fw_update_cmd_tbl[] = {};
+static uint8_t query_device_identifiers(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
+					uint16_t *resp_len, void *ext_params)
+{
+	if (!mctp_inst || !buf || !resp || !resp_len)
+		return PLDM_ERROR;
+
+	return PLDM_SUCCESS;
+}
+
+static uint8_t get_firmware_parameters(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
+				       uint16_t *resp_len, void *ext_params)
+{
+	if (!mctp_inst || !buf || !resp || !resp_len)
+		return PLDM_ERROR;
+
+	return PLDM_SUCCESS;
+}
+
+static uint8_t request_update(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
+			      uint16_t *resp_len, void *ext_params)
+{
+	if (!mctp_inst || !buf || !resp || !resp_len)
+		return PLDM_ERROR;
+
+	struct _req_update_req *req_p = (struct _req_update_req *)buf;
+	struct _req_update_resp *resp_p = (struct _req_update_resp *)resp;
+
+	resp_p->completion_code = PLDM_BASE_CODES_SUCCESS;
+
+	resp_p->fw_dev_meta_data_len = 0x0000; // currently not support meta data
+
+	if (req_p->pkg_data_len)
+		resp_p->fd_will_send_get_pkg_data_cmd = 0x01;
+	else
+		resp_p->fd_will_send_get_pkg_data_cmd = 0x00;
+
+	*resp_len = sizeof(struct _req_update_resp);
+	return PLDM_SUCCESS;
+}
+
+static uint8_t pass_component_table(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
+				    uint16_t *resp_len, void *ext_params)
+{
+	if (!mctp_inst || !buf || !resp || !resp_len)
+		return PLDM_ERROR;
+
+	return PLDM_SUCCESS;
+}
+
+static uint8_t update_component(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
+				uint16_t *resp_len, void *ext_params)
+{
+	if (!mctp_inst || !buf || !resp || !resp_len)
+		return PLDM_ERROR;
+
+	return PLDM_SUCCESS;
+}
+
+static uint8_t activate_firmware(void *mctp_inst, uint8_t *buf, uint16_t len, uint8_t *resp,
+				 uint16_t *resp_len, void *ext_params)
+{
+	if (!mctp_inst || !buf || !resp || !resp_len)
+		return PLDM_ERROR;
+
+	return PLDM_SUCCESS;
+}
+
+static pldm_cmd_handler pldm_fw_update_cmd_tbl[] = {
+	{ PLDM_FW_UPDATE_CMD_CODE_QUERY_DEVICE_IDENTIFIERS, query_device_identifiers },
+	{ PLDM_FW_UPDATE_CMD_CODE_GET_FIRMWARE_PARAMETERS, get_firmware_parameters },
+	{ PLDM_FW_UPDATE_CMD_CODE_REQUEST_UPDATE, request_update },
+	{ PLDM_FW_UPDATE_CMD_CODE_PASS_COMPONENT_TABLE, pass_component_table },
+	{ PLDM_FW_UPDATE_CMD_CODE_UPDATE_COMPONENT, update_component },
+	{ PLDM_FW_UPDATE_CMD_CODE_ACTIVE_FIRMWARE, activate_firmware },
+};
 
 uint8_t pldm_fw_update_handler_query(uint8_t code, void **ret_fn)
 {
--- common/service/pldm/pldm_fw_update.h
+++ common/service/pldm/pldm_fw_update.h
@@ -5,8 +5,11 @@
 #include <stdint.h>
 
 enum {
+	/* inventory commands */
 	PLDM_FW_UPDATE_CMD_CODE_QUERY_DEVICE_IDENTIFIERS = 0x01,
 	PLDM_FW_UPDATE_CMD_CODE_GET_FIRMWARE_PARAMETERS = 0x02,
+
+	/* update commands */
 	PLDM_FW_UPDATE_CMD_CODE_REQUEST_UPDATE = 0x10,
 	PLDM_FW_UPDATE_CMD_CODE_PASS_COMPONENT_TABLE = 0x13,
 	PLDM_FW_UPDATE_CMD_CODE_UPDATE_COMPONENT = 0x14,
@@ -19,6 +22,69 @@ enum {
 	PLDM_FW_UPDATE_CMD_CODE_CANCEL_UPDATE_COMPONENT = 0x1C,
 	PLDM_FW_UPDATE_CMD_CODE_CANCEL_UPDATE = 0x1D,
 };
+
+enum str_type {
+	STR_TYPE_UNKNOWN,
+	STR_TYPE_ASCII,
+	STR_TYPE_UTF8,
+	STR_TYPE_UTF16_LE,
+	STR_TYPE_UTF16_BE,
+};
+
+enum trans_flag {
+	TRANS_START = 0x1,
+	TRANS_MIDDLE = 0x2,
+	TRANS_END = 0x4,
+	TRANS_START_END = 0x5,
+};
+
+enum comp_resp_code {
+	cc_00 = 0x00,
+	cc_01,
+	cc_02,
+	cc_03,
+	cc_04,
+	cc_05,
+	cc_06,
+	cc_07,
+	cc_08,
+	cc_09,
+	cc_0A,
+	cc_0B,
+};
+
+struct _req_update_req {
+	uint32_t max_trans_size;
+	uint16_t num_of_comp;
+	uint8_t max_ostd_trans_req;
+	uint16_t pkg_data_len;
+	uint8_t comp_img_set_ver_str_type;
+	uint8_t comp_img_set_ver_str_len;
+	char comp_img_set_ver_str[255];
+} __attribute__((packed));
+
+struct _req_update_resp {
+	uint8_t completion_code;
+	uint16_t fw_dev_meta_data_len;
+	uint8_t fd_will_send_get_pkg_data_cmd;
+} __attribute__((packed));
+
+struct _pass_comp_table_req {
+	uint8_t trans_flag;
+	uint16_t comp_class;
+	uint16_t comp_id;
+	uint8_t comp_class_idx;
+	uint32_t comp_comparison_stamp;
+	uint8_t comp_ver_str_type;
+	uint8_t comp_ver_str_len;
+	char comp_ver_str[255];
+} __attribute__((packed));
+
+struct _pass_comp_table_resp {
+	uint8_t completion_code;
+	uint8_t comp_resp;
+	uint8_t comp_resp_code;
+} __attribute__((packed));
 
 uint8_t pldm_fw_update_handler_query(uint8_t code, void **ret_fn);
 
