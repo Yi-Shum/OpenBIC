e99c1aed5129a4770341e718a533b3305a5059ea
Eason Chen <EasonChen1@quantatw.com>
2022-07-01 13:18:08 +0800

    Add interrupt of post code complete

--- meta-facebook/yv35-hd/src/platform/plat_isr.c
+++ meta-facebook/yv35-hd/src/platform/plat_isr.c
@@ -5,6 +5,11 @@
 #include "power_status.h"
 #include "ipmi.h"
 
+void ISR_POST_COMPLETE()
+{
+	set_post_status(FM_BIOS_POST_CMPLT_BIC_N);
+}
+
 K_WORK_DELAYABLE_DEFINE(set_DC_on_5s_work, set_DC_on_delayed_status);
 K_WORK_DELAYABLE_DEFINE(set_DC_off_10s_work, set_DC_off_delayed_status);
 #define DC_ON_5_SECOND 5
